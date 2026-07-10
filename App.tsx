0037dce9261a9b286ceaf4070c29ead12408e4c1
HERYORDEJY <heryordejy.dev@gmail.com>
2026-07-11 00:29:51 +0100

    `feat(font): integrate Google Sans font and splash screen`
    * Add Google Sans font support with loading mechanism
    * Implement splash screen handling during font loading
    * Create font.ts for font management

--- App.tsx
+++ App.tsx
@@ -1,8 +1,29 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import RootNavigator from "~/navigation/RootNavigator";
 import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+    const [loaded, error] = useFonts({
+        GoogleSansBold: require("./src/assets/fonts/GoogleSans-Bold.ttf"),
+        GoogleSansMedium: require("./src/assets/fonts/GoogleSans-Medium.ttf"),
+        GoogleSansRegular: require("./src/assets/fonts/GoogleSans-Regular.ttf"),
+    });
+
+    useEffect(() => {
+        if (loaded || error) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded, error]);
+
+    if (!loaded && !error) {
+        return null;
+    }
+
     return (
         <SafeAreaProvider>
             <NavigationContainer>
---
+++ src/types/font.ts
@@ -0,0 +1,7 @@
+export const GoogleSansFont = {
+    regular: "GoogleSansRegular",
+    medium: "GoogleSansMedium",
+    bold: "GoogleSansBold",
+};
+
+export type FontType = keyof typeof GoogleSansFont;
